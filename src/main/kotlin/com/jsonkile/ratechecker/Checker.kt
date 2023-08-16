dc400a2c2438f7d4ae3203d53feb6f67ec1766eb
jsonkile <kilejayson@gmail.com>
2023-08-16 15:48:25 +0100

    added user agent

--- src/main/kotlin/com/jsonkile/ratechecker/Checker.kt
+++ src/main/kotlin/com/jsonkile/ratechecker/Checker.kt
@@ -6,12 +6,16 @@ import org.jsoup.Jsoup
 class Checker(val fromCurrency: Currency, val toCurrency: Currency) {
 
     private val jsoup: Connection = Jsoup.connect("https://www.google.com/search?q=${fromCurrency}+to+${toCurrency}")
+        .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36").timeout(7000)
 
-    /**
-     * @throws IllegalArgumentException if the conversion rate could not be found
-     */
-    operator fun invoke(): Double {
-        val document = jsoup.get()
+            /**
+             * @throws IllegalArgumentException if the conversion rate could not be found
+             */
+        operator
+
+    fun invoke(): Double {
+        val document =
+            jsoup.get()
         val elementWithRateInfo = requireNotNull(document.getElementsByAttribute("data-exchange-rate").first()) {
             "The conversion rate could not be found. Please try another currency combination."
         }
